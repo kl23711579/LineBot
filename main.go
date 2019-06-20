f6167ab74d2db26f0d570cd2d0f4584eb5d939a4
kl23711579 <kl23711579@gmail.com>
2019-06-20 15:25:03 +1000

    Rubbish message push

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/line/line-bot-sdk-go/linebot"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -12,9 +13,6 @@ func main() {
 	channelSecret := "cc8b2d517c550675def314b00543a098"
 	channelToken := "nyKC/WBqxvVrx15ftpm7+GGSlaRY1BQjDh/vV712PL3iAvJ0UCyqE3Nz5MIhuaymAvB+DP8v17IDzAlaUNYJ5CpOTa8ByRlYVtYS5Sxusd2EUvuPSYo7zvndX09RTSKPLqmLYtl91X7JT7cLqRC2YAdB04t89/1O/w1cDnyilFU="
 	userId := "U2ccabf42f930672b3187c9e21e3cb52a"
-
-	weatherDetail := GetData()
-	fmt.Println(weatherDetail)
 
 	bot, err := linebot.New(
 		channelSecret,
@@ -24,10 +22,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if _, err := bot.PushMessage(userId, linebot.NewTextMessage(weatherDetail)).Do(); err != nil {
-		fmt.Println("err    ", err)
-		log.Fatal(err)
-	}
+	go Weather(userId, bot)
+	go Rubbish(userId, bot)
 
 	// Setup HTTP Server for receiving requests from LINE platform
 	http.HandleFunc("/webhook", func(w http.ResponseWriter, req *http.Request) {
@@ -58,3 +54,36 @@ func main() {
 	}
 }
 
+func Weather(userId string, bot *linebot.Client) {
+	fiveMTick := time.Tick(5 * time.Minute)
+
+	for t := range fiveMTick {
+		hour, minute, _ := t.Clock()
+		if hour == 7 && minute >= 0 && minute <= 5 {
+			weatherDetail := GetData()
+			fmt.Println(weatherDetail)
+			if _, err := bot.PushMessage(userId, linebot.NewTextMessage(weatherDetail)).Do(); err != nil {
+				fmt.Println("err    ", err)
+				log.Fatal(err)
+			}
+		} else {
+			fmt.Println("Time no OK")
+		}
+	}
+}
+
+func Rubbish(userId string, bot *linebot.Client) {
+	hourTick := time.Tick(time.Hour)
+
+	for t := range hourTick {
+		weekDay := t.Weekday()
+		fmt.Println(weekDay)
+		hour := t.Hour()
+		if int(weekDay) == 1 && hour >= 7 && hour < 8{
+			if _, err := bot.PushMessage(userId, linebot.NewTextMessage("Take out rubbish.")).Do(); err != nil {
+				fmt.Println("Rubbish err   ", err)
+				log.Fatal(err)
+			}
+		}
+	}
+}
